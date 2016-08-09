939ee3bebad8c0639f8878f8f11cc6c6f6d19920
Kurt <breath103@gmail.com>
2016-08-09 19:31:24 +0900

    1. remove redundant Promises 2. update Component.status

--- src/worker/models/component.js
+++ src/worker/models/component.js
@@ -18,54 +18,57 @@ export default class Component extends BaseModel {
   }
 
   watch() {
-    Promise.all([this.getWatchers(), this.getNotifiers()])
-      .then((argv) => {
-        const [watchers, notifiers] = argv;
-        Promise.all(watchers.map(w => w.watch()))
-          .then(results => {
-            const watchResult = results.find(r => (r.status === STATUS_ERROR));
-            if (watchResult) {
-              notifiers.forEach(n => {
-                n.notify(watchResult).then();
-              });
-            } else {
-              notifiers.forEach(n => {
-                n.notify(results[0]).then();
-              });
-            }
+    return Promise.all([
+      this.getWatchers(),
+      this.getNotifiers(),
+    ]).then((argv) => {
+      const [watchers, notifiers] = argv;
+      return Promise.all(watchers.map(w => w.watch()))
+        .then(results => {
+          const watchResult =
+            results.find(r => (r.status === STATUS_ERROR)) || results[0];
+
+          this.status = watchResult;
+          notifiers.forEach(n => {
+            n.notify(watchResult).then();
           });
-      });
+
+          return this.status;
+        });
+    });
   }
 
   getNotifiers() {
-    return new Promise(resolve => {
-      BaseModel.db()
-        .cypher(`MATCH (c:Component)-[:NOTIFY]->(n:Notifier) 
-                WHERE id(c) = ${this.id} RETURN id(n), n`)
-        .then(resp => {
-          const result = resp.body.results[0].data;
-          resolve(result.map(r => {
-            const id = r.row[0];
-            let notifier = notifierPool[id];
-            if (!notifier) {
-              notifier = new notifierTypes[r.row[1].type](id, r.row[1]);
-              notifierPool[id] = notifier;
-            }
-            return notifier;
-          }));
+    return BaseModel.db()
+      .cypher(`
+        MATCH (c:Component)-[:NOTIFY]->(n:Notifier)
+        WHERE id(c) = ${this.id} RETURN id(n), n
+      `).then(resp => {
+        const result = resp.body.results[0].data;
+        const notifiers = result.map(r => {
+          const id = r.row[0];
+          let notifier = notifierPool[id];
+          if (!notifier) {
+            notifier = new notifierTypes[r.row[1].type](id, r.row[1]);
+            notifierPool[id] = notifier;
+          }
+          return notifier;
         });
-    });
+
+        return notifiers;
+      });
   }
 
   getWatchers() {
-    return new Promise(resolve => {
-      BaseModel.db()
-        .cypher(`MATCH (c:Component)<-[:WATCH]-(n:Watcher) 
-                  WHERE id(c) = ${this.id} RETURN id(n), n`)
-        .then(resp => {
-          const result = resp.body.results[0].data;
-          resolve(result.map(r => new watcherTypes[r.row[1].type](r.row[0], r.row[1])));
-        });
-    });
+    return BaseModel.db()
+      .cypher(`
+        MATCH (c:Component)<-[:WATCH]-(n:Watcher)
+        WHERE id(c) = ${this.id} RETURN id(n), n
+      `).then(resp => {
+        const result = resp.body.results[0].data;
+        const watchers = result.map(r => new watcherTypes[r.row[1].type](r.row[0], r.row[1]));
+
+        return watchers;
+      });
   }
 }
